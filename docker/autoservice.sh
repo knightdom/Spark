52ad9f932eebff7dd1c6bc0b9049bf80352d57b2
方得着 <kinghtdom@outlook.com>
2022-05-13 00:24:39 +0800

    生成hadoop集群

---
+++ docker/autoservice.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "172.18.0.2 node1 node1.kinghtdom.com" >> /etc/hosts
+echo "172.18.0.3 node2 node2.kinghtdom.com" >> /etc/hosts
+echo "172.18.0.4 node3 node3.kinghtdom.com" >> /etc/hosts
+
+/bin/sh
--- docker/down.sh
+++ docker/down.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-docker stop hadoop3.3-cluster
-docker rm hadoop3.3-cluster
+docker stop hadoop01 hadoop02 hadoop03
+docker rm hadoop01 hadoop02 hadoop03
 docker build -t kinghtdom/hadoop3.3-cluster .
-docker-compose up
+docker network create --driver=bridge --subnet 172.18.0.0/24 hadoop
+
+docker run -it --name hadoop01 \
+--network hadoop \
+--ip 172.18.0.2 \
+-h node1.kinghtdom.com \
+-p 9870:9870 \
+-p 8088:8088 \
+-v /Users/edgar/Documents/www/BigData:/www/ \
+-d kinghtdom/hadoop3.3-cluster
+
+docker run -it --name hadoop02 \
+--network hadoop \
+--ip 172.18.0.3 \
+-h node2.kinghtdom.com \
+-v /Users/edgar/Documents/www/BigData:/www/ \
+-d kinghtdom/hadoop3.3-cluster
+
+docker run -it --name hadoop03 \
+--network hadoop \
+--ip 172.18.0.4 \
+-h node3.kinghtdom.com \
+-v /Users/edgar/Documents/www/BigData:/www/ \
+-d kinghtdom/hadoop3.3-cluster
